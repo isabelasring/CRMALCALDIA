1998252395bd057f61a633bd14fb62063dc52f38
Geotrends SAS <geotrends@Mac-mini-de-Geotrends-2.local>
2026-06-25 15:06:12 -0500

    errores

---
+++ espocrm-custom/files/client/custom/src/views/case/list.js
@@ -0,0 +1,21 @@
+define('custom:views/case/list', ['views/list'], function (Dep) {
+
+    return Dep.extend({
+
+        setup: function () {
+            Dep.prototype.setup.call(this);
+
+            if (!this.getAcl().check(this.scope, 'create')) {
+                this.menu = this.menu || {};
+
+                var hidden = this.menu.hiddenItemList || [];
+
+                if (hidden.indexOf('create') === -1) {
+                    hidden.push('create');
+                }
+
+                this.menu.hiddenItemList = hidden;
+            }
+        },
+    });
+});
